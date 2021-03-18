aa74e058c23c217c9bb3b0d6d97f55e54de56d5f
lyx <canoecanoe@aliyun.com>
2021-03-18 17:57:04 +0800

    29 柱子2

--- js/index.js
+++ js/index.js
@@ -22,19 +22,24 @@
     xAxis: [
         {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            data: ['旅游行业', '教育培训', '游戏行业', '医疗行业', '电商行业', '社交行业', '金融行业'],
             axisTick: {
                 alignWithLabel: true
             },
             // 修改刻度标签，相关样式
             axisLabel: {
               color: "rgba(255,255,255,.6)",
-              fontSize: '12'
+              fontSize: '10',
+              interval: 0, // 横轴信息全部显示
+              rotate: 30, // 30度角倾斜显示，也可-30度
+              
             },
             // 不显示x坐标轴样式
             axisLine: {
               show: false
-            }
+            },
+            showMinLabel: true,//显示最小值
+            showMaxLabel: true,//显示最大值
 
         }
     ],
@@ -66,11 +71,85 @@
         {
             name: '直接访问',
             type: 'bar',
-            barWidth: '60%',
-            data: [10, 52, 200, 334, 390, 330, 220]
+            barWidth: '35%',
+            data: [200, 300, 300, 900, 1500, 1200, 600],
+            itemStyle: {
+              normal: {
+                barBorderRadius: 2,       
+              }
+          },
         }
     ]
 };
 // 把配置项给实例对象
 myChart.setOption(option)
+// 图表适配页面
+window.addEventListener("resize", function() {
+  myChart.resize();
+})
+})();
+// 柱状图2
+(function(){
+  // 实例化对象
+  var myChart = echarts.init(document.querySelector(".bar2 .chart"));
+  // 指定配置和数据
+  var option = {
+    grid: {
+      top: "10%",
+      left: "7%",
+      bottom: "10%",
+      containLabel: true
+    },
+	// 不显示x轴的相关信息
+    xAxis: {
+        show: false,
+    },
+    yAxis: {
+        type: 'category',
+        data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)'],
+		// 不显示y轴的线
+		axisLabel: {
+			show: false
+		},
+		// 不显示刻度
+		axisTick: {
+			show: false
+		},
+		// 刻度标签里的文字设置成白色
+		axisLabel: {
+			color: "#fff"
+		}
+    },
+    series: [
+        {
+            name: '条',
+            type: 'bar',
+            data: [18203, 23489, 29034, 104970, 131744, 630230],
+			// 修改第一组柱子的圆角
+			itemStyle: {
+				barBorderRadius: 20
+			},
+			// 柱子之间的距离
+			barCategoryGap: 50,
+			//柱子的宽度
+			barWidth: 10,
+			// 显示柱子内文字
+			label: {
+				show: true,
+				position: "inside",
+				// 文字的显示格式,{c}为自动解析data里的数据
+				// {a}, {b}，{c}分别表示系列名，数据名，数据值
+				formatter: "{c}%" 
+			}
+        },
+        {
+            name: '2012年',
+            type: 'bar',
+            data: [19325, 23438, 31000, 121594, 134141, 681807]
+        }
+    ]
+};
+
+  // 把配置给实例对象
+  myChart.setOption(option);
 })();
